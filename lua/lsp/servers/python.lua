3dc3cbbdc47921ae8914a8d9479744b14b65c38b
Nguyen Pham Quang Dinh <destnguyxn@nexondv.com>
2023-03-27 18:14:10 +0700

    fix: add missing config file

---
+++ lua/lsp/servers/python.lua
@@ -0,0 +1,17 @@
+local M = {}
+local settings = {
+  pylsp = {
+        plugins = {
+          pylint = { enabled = true, executable = "pylint" },
+          pyflakes = { enabled = false },
+          pycodestyle = { enabled = false },
+          jedi_completion = { fuzzy = true },
+          pyls_isort = { enabled = true },
+          pylsp_mypy = { enabled = true },
+        },
+      },
+}
+
+M.settings = settings
+
+return M
