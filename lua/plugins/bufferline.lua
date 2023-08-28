4ae201bdf0a140613a7260799d7ffed0a3bdc845
destnguyxn <chromevi123@gmail.com>
2023-08-28 20:00:01 +0700

    fix: bufferline style

--- lua/plugins/bufferline.lua
+++ lua/plugins/bufferline.lua
@@ -8,9 +8,9 @@ bufferline.setup({
 		right_mouse_command = function(n)
 			require("mini.bufremove").delete(n, false)
 		end,
-		show_buffer_close_icons = false,
+		show_buffer_close_icons = true,
 		separator_style = { "|", "|" },
-		always_show_bufferline = true,
+		always_show_bufferline = false,
 		style_preset = bufferline.style_preset.no_italic,
 		numbers = function(opts)
 			return string.format("%s", opts.ordinal)
