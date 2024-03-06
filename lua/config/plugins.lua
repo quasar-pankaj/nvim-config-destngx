c086f2122dd776ca7822c4cd13f1b80d2fe664dd
Nguyen Pham Quang Dinh [destnguyxn] <destnguyxn@nexondv.com>
2024-03-06 14:37:49 +0700

    fix: add config model for neoai

--- lua/config/plugins.lua
+++ lua/config/plugins.lua
@@ -278,7 +278,16 @@ return {
       { "<leader>as", desc = "summarize text" },
       { "<leader>ag", desc = "generate git message" },
     },
-    config = true,
+    config = function ()
+      require("neoai").setup({
+        models = {
+          {
+            name = "openai",
+            model = "gpt-4",
+          },
+        },
+      })
+    end,
   },
 
   -- LSP Cmp
